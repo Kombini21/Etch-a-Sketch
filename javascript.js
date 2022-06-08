8242ba6abcb513032b77f81b05953baa92633311
Gabriel Terrazas Correa <gabrielterrazas21@gmail.com>
2022-06-08 13:20:30 -0400

    Center all alements and create clean grid function

--- javascript.js
+++ javascript.js
@@ -1,26 +1,40 @@
-const container = document.getElementById("grid16");
+let container = document.getElementById("grid16");
 
 let rows = document.getElementsByClassName("gridRow");
 let cells = document.getElementsByClassName("cell");
 
 
 
+function cleanGrid() {
+
+    let romws = document.getElementById("grid16");
+    romws.innerHTML = "";
 
 
-defaultGrid();
-
-for (let g = 0; g < cells.length; g++) {
-
-    (cells.item(g)).addEventListener('mouseover', () => (cells.item(g)).classList.add("colorn"));
 }
+
 
 
 function defaultGrid() {
 
-    makeRows(24);
-    makeColumns(24);
 
+    var xn = document.getElementById('xname').value;
+    var yn = document.getElementById('yname').value;
+
+    if (yn > 64 || xn > 64 ) {
+        console.log("The value has to be below 64")
+    }
+    else{
+        makeRows(xn);
+        makeColumns(yn);
+
+        for (let g = 0; g < cells.length; g++) {
+
+            (cells.item(g)).addEventListener('mouseover', () => (cells.item(g)).classList.add("colorn"));
+        }
+    }
 }
+
 
 
 function makeRows(rowNum) {
